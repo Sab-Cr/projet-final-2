9839c6813e30f7d0c1b5411b740d1a9781234cf8
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-07-18 16:21:35 -0400

    Create fetch-request util

---
+++ client/src/utils/fetch-request.js
@@ -0,0 +1,16 @@
+// utility function to make fetch request
+// purpose: avoid entering the try-catch block everytime
+
+const fetchRequest = async(api) => {
+  try {
+    const data = await api();
+    if (data.error) throw new Error(data.error);
+    return data;
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default fetchRequest;
+
