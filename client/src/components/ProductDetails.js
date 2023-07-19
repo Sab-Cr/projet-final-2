7b0febf82eeb12c79316bb0bf36d8e797ec98e78
Viet Nguyen <19592926+v-ngu@users.noreply.github.com>
2023-07-18 23:08:21 -0400

    Fetch product details

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { styled } from "styled-components";
+import fetchRequest from "../utils/fetch-request";
+import { getProduct } from "../services/api";
+
 const ProductDetails = () => {
-    return (
+  const { productId } = useParams();
   
-      <div>ProductDetails</div>
+  // states
+  const [product, setProduct] = useState({});
+  const { 
+    _id,
+    name, 
+    price,
+    category,
+    imageSrc 
+  } = product;
   
-    );
-  };
-  
-  export default ProductDetails;
+  // get product details from server
+  useEffect(() => {
+    (async () => {
+      const res = await fetchRequest(() => getProduct(productId));
+      setProduct(res.data)
+    })();
+  }, [setProduct, productId]);
+
+  return (
+    <div>
+      <Img src={imageSrc} />
+      <p>{_id}</p>
+      <p>{name}</p>
+      <p>{price}</p>
+      <p>{category}</p>
+    </div>
+  );
+};
+
+export default ProductDetails;
+
+const Img = styled.img`
+  width: 150px;
+`;
